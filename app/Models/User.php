4e75480f9ac8578723bbf776b329609a1bd87d6e
teguhpermadi <tguh.permadi@gmail.com>
2025-05-18 22:37:56 +0700

    feat(auth): enable Sanctum authentication and update token migration
    - Added `Route::restifyAuth()` to enable authentication routes
    - Uncommented `auth:sanctum` middleware in Restify config
    - Added `HasApiTokens` trait to User model
    - Updated personal access tokens migration to use ULID for tokenable_id and added composite index

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasUlids;
+    use HasFactory, Notifiable, HasUlids, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
