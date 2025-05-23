d7f33410e63c1a983529b16b15af057453c378e5
teguhpermadi <tguh.permadi@gmail.com>
2025-05-24 04:23:16 +0700

    feat(faker): add FakerPicsumImagesProvider for generating placeholder images
    This commit introduces the `FakerPicsumImagesProvider` to generate placeholder images using the Picsum service. It includes:
    - Adding the `smknstd/fakerphp-picsum-images` dependency.
    - Registering the `FakerServiceProvider` in non-production environments.
    - Configuring a new `media` filesystem disk for storing generated images.
    - Updating the `media-library` configuration to use the `media` disk.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-
+    App\Providers\FakerServiceProvider::class,
     App\Providers\RestifyServiceProvider::class,
 ];
--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,14 @@ return [
             'report' => false,
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root'   => storage_path('app/public/media'),
+            'url'    => env('APP_URL').'/media',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
     ],
 
     /*
